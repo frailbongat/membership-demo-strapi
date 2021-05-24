1a3c30c9b1677c5cdb977e8f9ec3253117d6aa60
Frail Bongat <frail.bongat@gmail.com>
2021-05-24 08:39:47 +0800

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3af628da52a3cdb5587b944490e51b5a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '90c6bd49-42fe-4626-b229-370e869d3a18'
+};
